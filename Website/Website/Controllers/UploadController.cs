b39fd37c12534cd35c14ab5e060275a062ab73f7
lucykur <kallivayalil.family@gmail.com>
2011-09-26 20:07:26 +0530

    Adding upload of files for committee <Lucy>

---
+++ Website/Website/Controllers/UploadController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Web.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    [HandleError]
+    public class UploadController : Controller
+    {
+        private const string SessionKey = "employees";
+
+        private ICollection<UploadModel> Model
+        {
+            get
+            {
+                var employees = (ICollection<UploadModel>)Session[SessionKey];
+                if (employees == null)
+                {
+                    employees = new List<UploadModel>
+                    {
+                        new UploadModel
+                        {
+                            Id = "1",
+                            Name = "John Doe",
+                            Picture = "johndoe.jpg"
+                        }
+                    };
+
+                    Model = employees;
+                }
+
+                return employees;
+            }
+
+            set
+            {
+                Session[SessionKey] = value;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(Model);
+        }
+
+        [HttpPost]
+        public ActionResult Save(string id, HttpPostedFileBase attachment)
+        {
+            var attachmentName = Path.GetFileName(attachment.FileName);
+            var imagesPath = Server.MapPath("~/Content/UserFiles");
+            attachment.SaveAs(Path.Combine(imagesPath, attachmentName));
+
+            return Json(new { fileName = attachmentName }, "text/plain");
+        }
+
+        [GridAction]
+        public ActionResult Select()
+        {
+            return View(new GridModel(Model));
+        }
+
+        [HttpPost]
+        [GridAction]
+        public ActionResult Update(string id)
+        {
+            var employee = Model.First(e => e.Id == id);
+            TryUpdateModel(employee);
+
+            return View(new GridModel(Model));
+        }
+
+        [HttpPost]
+        [GridAction]
+        public ActionResult Insert(string lastUpload)
+        {
+            var employee = new UploadModel();
+            if (TryUpdateModel(employee))
+            {
+                employee.Id = Guid.NewGuid().ToString();
+                Model.Add(employee);
+            }
+
+            return View(new GridModel(Model));
+        }
+
+        [HttpPost]
+        [GridAction]
+        public ActionResult Delete(int id)
+        {
+            // The parameter of the Remove action must be called "fileNames"
+//            foreach (var fullName in fileNames)
+//            {
+//                var fileName = Path.GetFileName(fullName);
+//                var physicalPath = Path.Combine(Server.MapPath("~/Content/UserFiles"), fileName);
+//
+                // TODO: Verify user permissions
+//                if (System.IO.File.Exists(physicalPath))
+//                {
+                    // The files are not actually removed in this demo
+//                     System.IO.File.Delete(physicalPath);
+//                }
+//            }
+            // Return an empty string to signify success
+            return Content("");
+        }
+
+    }
+}
---
+++ Website/Website/Models/UploadModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Website.Models
+{
+    public class UploadModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        [UIHint("Attachment")]
+        public string Picture { get; set; }
+    }
+}
